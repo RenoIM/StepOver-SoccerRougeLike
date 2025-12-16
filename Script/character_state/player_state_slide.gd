65a5390c3bcf5441a23f73d5226e6d4b52550081
RenoIM <renomaulana234@gmail.com>
2025-12-16 09:52:34 +0700

    Adding Slide Friction

--- Script/character_state/player_state_slide.gd
+++ Script/character_state/player_state_slide.gd
@@ -1,14 +1,23 @@
 class_name PlayerStateSlide
 extends PlayerState
 
-var DURATION_SLIDE := 200
-
+var DURATION_SLIDE := 300
+var SLIDE_MULTIPLIER := 2.0
+var DECELERATION := 12.0 # higher = faster stop
 var time_start_slide := Time.get_ticks_msec()
+var slide_direction := Vector2.ZERO
 
 func _enter_tree() -> void:
+	var direction := KeyUtils.get_input_vector(player.control_scheme)
 	player.animation_player.play("slide")
 	time_start_slide = Time.get_ticks_msec()
+	player.velocity = direction * player.speed * SLIDE_MULTIPLIER
 	
-func _process(_delta: float) -> void:
-	if Time.get_ticks_msec() - time_start_slide > DURATION_SLIDE:
+func _process(delta: float) -> void:
+	# Smoothly reduce velocity toward zero
+	player.velocity = player.velocity.lerp(Vector2.ZERO, DECELERATION * delta)
+	# End slide if slow enough OR duration exceeded
+	if player.velocity.length() < 10 \
+	or Time.get_ticks_msec() - time_start_slide > DURATION_SLIDE:
+		player.velocity = Vector2.ZERO
 		state_transition_requested.emit(Player.State.MOVING)
